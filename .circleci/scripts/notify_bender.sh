d7f2dad69e904e66021f04923105da1dd0d96adb
Tomasz Cichowicz <cichowicz@gmail.com>
2020-07-28 21:44:30 +0200

    Simplify CircleCI config (#3)

--- .circleci/scripts/notify_bender.sh
+++ .circleci/scripts/notify_bender.sh
@@ -7,4 +7,4 @@ wget -O- \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $HUBOT_ACCESS_TOKEN" \
   --post-data "{\"serviceName\":\"$SERVICE_NAME\", \"environment\":\"dev\", \"image\":\"eu.gcr.io/$PROJECT_NAME/$SERVICE_NAME:$CIRCLE_SHA1\", \"initiator\":\"$CIRCLE_USERNAME\"}" \
-  $HUBOT_DEPLOY_URL
+  https://join-dev.com/hubot/deploy
